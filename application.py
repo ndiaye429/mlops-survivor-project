ed97c98b17b433c523ebb1dd4fb0e9791b30b3d2
ndiaye <ndiaye>
2026-04-21 01:04:59 +0200

    Data drift detected

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from flask import Flask,render_template,request,jsonify
 from src.logger import get_logger
+from alibi_detect.cd import KSDrift
 from src.feature_store import RedisFeatureStore
 from sklearn.preprocessing import StandardScaler
 
@@ -18,6 +19,22 @@ with open(MODEL_PATH , 'rb') as model_file:
 
 FEATURE_NAMES = ['age', 'fare', 'pclass', 'sex', 'embarked', 'Familysize', 'Isalone',
        'HasCabin', 'Title', 'Pclass_fare', 'age_fare']
+
+feature_store = RedisFeatureStore()
+scaler = StandardScaler()
+
+def fit_scaler_on_ref_data():
+    entity_ids = feature_store.get_all_entity_ids()
+    all_features = feature_store.get_batch_features(entity_ids)
+
+    all_features_df = pd.DataFrame.from_dict(all_features , orient='index')[FEATURE_NAMES]
+
+    scaler.fit(all_features_df)
+    return scaler.transform(all_features_df)
+
+
+historical_data = fit_scaler_on_ref_data()
+ksd = KSDrift(x_ref=historical_data , p_val=0.05)
 
 
 @app.route('/')
@@ -43,8 +60,22 @@ def predict():
 
         features = pd.DataFrame([[Age,Fare,Pclass,Sex,Embarked,Familysize,Isalone,HasCabin,Title,Pclass_Fare,Age_Fare]] , columns=FEATURE_NAMES)
 
+        ##### Data Drift Detection
+        features_scaled = scaler.transform(features)
+
+        drift = ksd.predict(features_scaled)
+        print("Drift Response : ",drift)
+
+        drift_response = drift.get('data',{})
+        is_drift = drift_response.get('is_drift' , None)
+
+        if is_drift is not None and is_drift==1:
+            print("Drift Detected....")
+            logger.info("Drift Detected....")
+
 
         prediction = model.predict(features)[0]
+    
 
         result = 'Survived' if prediction==1 else 'Did Not Survive'
 
@@ -52,10 +83,9 @@ def predict():
     
     except Exception as e:
         return jsonify({'error' : str(e)})
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+    
+    
+if __name__ =="__main__":
+    app.run()
 
 
